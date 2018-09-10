10017e8cf9b857ab6ce43ad85a9cb2cee1557987
nimwin <seyilowo@gmail.com>
2018-09-10 22:58:47 +0100

    updated the functionality of some of the arithmetic operators

--- project_3.js
+++ project_3.js
@@ -1,7 +1,6 @@
-function display(val)
-		{
+function display(val){
 		document.getElementById("d3").innerHTML+=val;
-		};
+}
 
 function e(){
 	try{
@@ -10,16 +9,59 @@ function e(){
 		}
 	catch(e) 
 			{
-			  c('Error') 
+			  c('Error'); 
 			}
 }
 
 function c(val) {
 		document.getElementById("d3").innerHTML=val;
-		}
+}
 
 
 function sqrt1(){
 	var x =  Math.sqrt(document.getElementById("d3").innerHTML);
 	document.getElementById("d3").innerHTML = x;
-};
+}
+
+function deleteChar() {
+	var x = document.getElementById("d3").innerHTML;
+	var y = x.substring(0, x.length - 1);
+	document.getElementById("d3").innerHTML = y;
+}
+
+function percent() {
+	var x = document.getElementById("d3").innerHTML;
+	var y = x * 100;
+	alert (x + "," + y);
+	document.getElementById("d3").innerHTML = y;
+}
+
+function toggle() {
+	var x = document.getElementById("d3").innerHTML;
+	if (x.substring(0, 1) == "-") {
+		var y = x.substring(1, x.length);
+		document.getElementById("d3").innerHTML = y;
+	} else {
+		document.getElementById("d3").innerHTML = "-" + x;
+	}
+}
+
+
+function leftParen() {
+    document.getElementById("d3").innerHTML += "*(" ;
+
+}
+
+// function exp() {
+	
+// 	var exp = '^(' ;
+// 	document.getElementById("d3").innerHTML += exp;
+// }
+
+// function leftParen() {
+//     var x = document.getElementById("d3") ;
+//     var y = document.getElementById("myPara") ;
+//     x.innerText += "(" ;
+//     y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ? 
+//     " * (" : "(" ;
+// }
